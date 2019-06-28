03b9b0ce56a7f51b1638128e222bfc130004afa7
chenwenxin <vinson2088@163.com>
2019-06-28 16:10:03 +0800

    init

---
+++ src/components/nav.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="nav">
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      background-color="#409eff"
+      text-color="#fff"
+      active-text-color="#fff"
+      @select="handleSelect">
+      <el-menu-item index="1">
+        <el-input
+          placeholder="请输入内容"
+          v-model="search">
+          <i slot="suffix" class="el-input__icon el-icon-search"></i>
+        </el-input>
+      </el-menu-item>
+      <el-menu-item index="2">
+        <i class="el-icon-house"></i>
+        <span slot="title">主页</span>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <i class="el-icon-user"></i>
+        <span slot="title">关于我</span>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <i class="el-icon-date"></i>
+        <span slot="title">博客</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'navigation',
+  data() {
+    return {
+      activeIndex: '2',
+      search: ''
+    }
+  },
+  methods: {
+    handleOpen() {},
+    handleClose() {},
+    handleSelect() {}
+  },
+}
+</script>
+<style lang="scss" scoped>
+.nav{
+  .el-menu{
+    display: flex;
+    justify-content: center;
+    .el-menu-item{
+      i{
+        color: #fff;
+      }
+      .el-input{
+        i{
+          color: #C0C4CC;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,88 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <navigation></navigation>
+    <el-row class="main" type="flex" justify="space-around">
+      <el-col :span="6">
+        <div class="box">
+          <div class="avatar">
+            <img src="./../assets/logo.png" alt="avatar" class="img">
+          </div>
+          <p class="name">Vinson</p>
+          <p class="introduction">君子终日乾乾  夕惕若厉  无咎</p>
+          <p>博客数</p>
+          <p class="num"><span>123</span></p>
+        </div>
+        <div class="box">
+          <el-tag effect="plain" v-for="(item, index) in tagList" :key="index">{{item.tag}}</el-tag>
+        </div>
+      </el-col>
+      <el-col :span="8">
+        
+      </el-col>
+      <el-col :span="6"></el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import navigation from './../components/nav'
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    navigation
+  },
+  data() {
+    return {
+      tagList: [
+        {tag: 'js'},
+        {tag: 'html'},
+        {tag: 'css'},
+        {tag: 'webpack'},
+        {tag: 'es6'},
+        {tag: 'Vue.js'},
+        {tag: 'jQuery'}
+      ]
+    }
   }
 }
 </script>
+<style lang="scss" scoped>
+.main{
+  background-color: #f0f0f0;
+  height: 100%;
+  .box{
+    background-color: #fff;
+    font-size: 16px;
+    margin: 3em 0;
+    .avatar{
+      position: relative;
+      background-color: #409eff;
+      height: 100px;
+      margin-bottom: 60px;
+      .img{
+        position: absolute;
+        left: 50%;
+        bottom: -40px;
+        transform: translateX(-50%);
+        width: 80px;
+        background-color: #fafafa;
+        border-radius: 50px;
+        border: 2px solid #fff;
+      }
+    }
+    .name{
+      font-size: 20px;
+      font-weight: bold;
+      margin: 10px 0;
+    }
+    .introduction{
+      color: #999;
+      margin: 0;
+      font-size: 12px;
+    }
+    .num{
+      padding-bottom: 40px;
+    }
+  }
+}
+</style>
